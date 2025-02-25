91e90f4cd72868607e49f7afd5ec448d6124d245
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2025-02-25 21:32:00 +0200

    Add task 3

---
+++ Task3.js
@@ -0,0 +1,68 @@
+function Book(
+  title,
+  numOfChapters,
+  author,
+  numOfPages,
+  publisher,
+  numOfCopies
+) {
+  this.title = title;
+  this.numOfChapters = numOfChapters;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.publisher = publisher;
+  this.numOfCopies = numOfCopies;
+
+  Book.CreatedBooksCount++;
+}
+
+Book.CreatedBooksCount = 0;
+
+Book.getCreatedBooksCount = function () {
+  return this.CreatedBooksCount;
+};
+
+function Box(height, width, length, material) {
+  this.height = height;
+  this.width = width;
+  this.length = length;
+  this.material = material;
+
+  this.content = [];
+}
+
+Box.prototype.addNewBook = function (
+  title,
+  numOfChapters,
+  author,
+  numOfPages,
+  publisher,
+  numOfCopies
+) {
+  const newBook = new Book(
+    title,
+    numOfChapters,
+    author,
+    numOfPages,
+    publisher,
+    numOfCopies
+  );
+  this.content.push(newBook);
+};
+
+Box.prototype.addBook = function (newBook) {
+  this.content.push(newBook);
+};
+
+Box.prototype.countBooks = function () {
+  return this.content.length;
+};
+
+Box.prototype.deleteBook = function (bookTitle) {
+  const foundIndex = this.content.findIndex((book) => book.title === bookTitle);
+  if (foundIndex !== -1) {
+    this.content.splice(foundIndex, 1);
+  }
+};
+
+
